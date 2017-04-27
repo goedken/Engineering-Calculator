4f479cd1fe885e41cc124f75050061387059ef4d
tcstevenson <tcstevenson@wisc.edu>
2017-04-26 20:56:14 -0500

    Quick Preference for home location
    created, variables do not save yet.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,44 +1,48 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicLoading, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+	// Form data for the login modal
+	$scope.streetAddress = "";
+	$scope.city = "";
+	$scope.state = "";
+	$scope.zipCode = "";
+	
+	//Load popup window layout from defaultHome.html
+	$ionicModal.fromTemplateUrl('templates/defaultHome.html', {
+		scope: $scope
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+	
+	//Closes popup
+	$scope.closeWindow = function() {
+		$scope.modal.hide();
+	};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+	//Opens popup
+	$scope.defaultHome = function() {
+		$scope.modal.show();
+	};
+	
+	//Save user entered data
+	$scope.save = function() {
+		$ionicLoading.show({ template: 'Saving', noBackdrop: true, duration: 1000 });
+		console.log($scope.streetAddress);
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+		// Simulate a login delay. Remove this and replace with your login
+		// code if using a login system
+		$timeout(function() {
+			$scope.closeWindow();
+		}, 1000);
+	};
 })
 
 .controller('appMainCtrl', function($scope) {
