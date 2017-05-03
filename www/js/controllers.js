2654988ca265adafd588b204831b0938655188d8
STEVENS-PC\Steven <radeztsky@wisc.edu>
2017-05-03 04:18:32 -0500

    added functionality for momentum, pvt, centAccel, weight

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,7 +41,9 @@ angular.module('starter.controllers', [])
 			motion: "Laws of Motion",
 			hookesLaw: "Hooke's Law",
 			pvt: "Pressure, Volume, Temperature",
-			weight: "Weight"
+			weight: "Weight",
+      momentum: "Momentum",
+      centAccel: "Centripetal Acceleration"
 	}
 
 	$scope.assignModal = function(event) {
@@ -59,6 +61,12 @@ angular.module('starter.controllers', [])
 		case ($scope.physics.weight):
 			$scope.template.url = 'templates/weight.html';
 		break;
+		case ($scope.physics.momentum):
+      $scope.template.url = 'templates/momentum.html';
+    break;
+    case ($scope.physics.centAccel):
+      $scope.template.url = 'templates/centAccel.html';
+    break;
 		default:
 			break;
 		}
@@ -109,10 +117,44 @@ angular.module('starter.controllers', [])
 	}
 	$scope.computeWeight = function(){
 		$scope.returned.state = "";
-		var m = $scope.value.one;
-		physFac.weight(m);
+		var w = $scope.value.one;
+		var m = $scope.value.two;
+		physFac.weight(w,m);
 		$scope.returned = physFac.value;
 	}
+  $scope.computeHookes = function(){
+    $scope.returned.state = "";
+    var f = $scope.value.one;
+    var k = $scope.value.two;
+    var x = $scope.value.three;
+    physFac.hookes(f,k,x);
+    $scope.returned = physFac.value;
+  }
+  $scope.computeMomentum = function(){
+    $scope.returned.state = "";
+    var p = $scope.value.one;
+    var m = $scope.value.two;
+    var v = $scope.value.three;
+    physFac.momentum(p,m,v);
+    $scope.returned = physFac.value;
+  }
+  $scope.computePVT = function(){
+    $scope.returned.state = "";
+    var p = $scope.value.one;
+    var v = $scope.value.two;
+    var n = $scope.value.three;
+    var t = $scope.value.four;
+    physFac.pvt(p,v,n,t);
+    $scope.returned = physFac.value;
+  }
+  $scope.computeCentAccel = function(){
+    $scope.returned.state = "";
+    var a = $scope.value.one;
+    var v = $scope.value.two;
+    var r = $scope.value.three;
+    physFac.centAccel(a,v,r);
+    $scope.returned = physFac.value;
+  }
 })
 
 .factory('generalFactory', function(){
@@ -192,17 +234,100 @@ angular.module('starter.controllers', [])
 			value.five = a;
 			value.six = t;
 		}
-		var weight = function(m){
-			if(m != ""){
-				value.one = m * 9.8;
-			} else {
-				value.state = "Minimum input required: mass";
-			}
-		}
+		var weight = function(w,m){
+      if (m != "") {
+        value.one = m * 9.8;
+      } else if (w != "") {
+        value.two = w / 9.8
+      } else {
+        value.state = "Minimum input required: one of the variables";
+      }
+    }
+    var hookes = function(f,k,x){
+      if(k != "" && x != ""){
+        value.one = (-k) * x;
+        value.two = k;
+        value.three = x;
+      } else if(f != "" && x != ""){
+        value.one = f;
+        value.two = f/-x;
+        value.three = x;
+      } else if(k != "" && k != ""){
+        value.one = f;
+        value.two = k;
+        value.three = f/(-k);
+      }else {
+        value.state = "Minimum input required: K Constant and Delta X";
+      }
+    }
+    var momentum = function(p,m,v){
+      if(m != "" && v != ""){
+        value.one = m * v;
+        value.two = m;
+        value.three = v;
+      } else if(p != "" && v != ""){
+        value.one = p;
+        value.two = p/v;
+        value.thee = v;
+      } else if(p != "" && m != ""){
+        value.one = p;
+        value.two = m;
+        value.three = p/m
+      } else {
+        value.state = "Minimum input required: mass and velocity";
+      }
+    }
+    var pvt = function(p,v,n,t){
+      r = 8.3145;
+      if (p != "" && v != "" && t != "") {
+        value.one = p;
+        value.two = v;
+        value.three = (p * v) / (r * t);
+        value.four = t;
+      } else if (p != "" && v != "" && n != "") {
+        value.one = p;
+        value.two = v;
+        value.three = n;
+        value.four = (p * v) / (n * r);
+      } else if (p != "" && n != "" && t != "") {
+        value.one = p;
+        value.two = (n * r * t) / p;
+        value.three = n;
+        value.four = t;
+      } else if (v != "" && n != "" && t != "") {
+        value.one = (n * r * t) / v;
+        value.two = v;
+        value.three = n;
+        value.four = t;
+      } else {
+        value.state = "Minimum input required: any three variables";
+      }
+    }
+    var centAccel = function(a,v,r){
+      if (v != "" && r != "") {
+        value.one = (v*v)/r;
+        value.two = v;
+        value.three = r;
+      } else if (a != "" && r != "") {
+        value.one = a;
+        value.two = Math.sqrt(a*r);
+        value.three = r;
+      } else if (a != "" && v != "") {
+        value.one = a;
+        value.two = v;
+        value.three = (v*v)/a;
+      }else {
+        value.state = "Minimum input required: any two variables";
+      }
+    }
 		return {
 			value: value,
 			lawsOfMotion: lawsOfMotion,
-			weight: weight
+			weight: weight,
+      hookes: hookes,
+      momentum: momentum,
+      pvt: pvt,
+      centAccel: centAccel
 		}
 	};
 	return physicsFactory;
